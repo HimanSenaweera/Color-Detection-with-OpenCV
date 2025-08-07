e6e7d041845f27c71a1d147a6c3e5dbf1500bcd2
HimanSenaweera <himan20021011@gmail.com>
2025-08-07 10:29:21 +0530

    completed the color detection algorithm

---
+++ main.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+from utils import get_limits
+
+yellow = [0, 255, 255]  # yellow in BGR colorspace
+lower_limit, upper_limit = get_limits(yellow)
+
+capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = capture.read()
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #here we are using the Hue channel of the hsv colorspace
+
+    # Mask for yellow
+    mask = cv2.inRange(hsv_frame, lowerb=lower_limit, upperb=upper_limit)
+    
+    
+    
+    # Clean the mask using morphological operations
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+    #cv2.imshow('mask', mask)
+    
+    # Find contours
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 500:  # only consider sufficiently large objects
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame) 
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -2,10 +2,12 @@ import numpy as np
 import cv2
 
 def get_limits(color):
-    c = np.uint8([[color]])  # Convert the BGR color to a format suitable for cvtColor
+    #Converts the input color into a 1×1×3 image required because cv2.cvtColor() needs an image-like array.
+    c = np.uint8([[color]])  
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
 
-    lowerLimit = hsvC[0][0][0] - 10, 100, 100
+    #hsvC[0][0][0] gives the Hue value.We create a range of ±10 around that Hue value.
+    lowerLimit = hsvC[0][0][0] - 10, 100, 100 
     upperLimit = hsvC[0][0][0] + 10, 255, 255
 
     lowerLimit = np.array(lowerLimit, dtype=np.uint8)
