6e0296c260ad9572936a6a94a31cc4b21437f329
Himan Senaweera <himan20021011@gmail.com>
2025-08-24 05:26:27 +0530

    Update utils.py

--- ColorDetection/utils.py
+++ ColorDetection/utils.py
@@ -3,7 +3,7 @@ import cv2
 
 def get_limits(color):
     #Converts the input color into a 1×1×3 image required because cv2.cvtColor() needs an image-like array.
-    c = np.uint8([[color]])  
+    c = np.array([[color]],dtype='uint8')  
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
 
     #hsvC[0][0][0] gives the Hue value.We create a range of ±10 around that Hue value.
